dcbd46ebf84069e1a443285f7541fb2f695403a4
bernardokawe <bernardokawe9@gmail.com>
2025-04-10 16:32:32 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class Ola {
     public static void main(String[] args) {
-     System.out.println("AYRTON SENNA SO BRASIL!");  
-        }
+     //System.out.println("AYRTON SENNA SO BRASIL!");  
+       JOptionPane.showMessageDialog(null,"AYRTON SENNA DO BRASIL");
+    }
     }
